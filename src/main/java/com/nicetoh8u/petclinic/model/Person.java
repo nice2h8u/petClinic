4fc4ae2581eef2a7a34f723f1f49296964274c5b
nice2h8u <nicetoh8u@gmail.com>
2018-11-09 12:59:10 +0300

    added JPA. closes #1

--- src/main/java/com/nicetoh8u/petclinic/model/Person.java
+++ src/main/java/com/nicetoh8u/petclinic/model/Person.java
@@ -6,20 +6,13 @@ import javax.persistence.GeneratedValue;
 
 
 
-public class Person {
+public class Person extends BaseEntity {
 
 
-    private Long id;
+
     private String firstName;
     private String lastName;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getFirstName() {
         return firstName;
--- src/main/java/com/nicetoh8u/petclinic/model/PetType.java
+++ src/main/java/com/nicetoh8u/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.nicetoh8u.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
